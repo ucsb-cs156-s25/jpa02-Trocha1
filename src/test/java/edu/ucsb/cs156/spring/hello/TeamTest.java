38aa5815369742aeb150f46f855bef2850dad157
Tristan Rocha <t_rocha@ucsb.edu>
2025-04-03 22:56:41 -0700

    xy - updated with tostring(), equals(), and hashcode() testing

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -1,6 +1,8 @@
 package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,6 +19,32 @@ public class TeamTest {
     @Test
     public void getName_returns_correct_name() {
        assert(team.getName().equals("test-team"));
+    }
+
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test
+    public void equals_returns_correct_bool() {
+        Team test_team = team;
+        assertTrue(team.equals(test_team), "True, test_team should point to the same object as team");
+        test_team = null;
+        assertFalse(team.equals(test_team), "False, test_team is not an instance of team");
+        test_team = new Team("new-team");
+        assertFalse(team.equals(test_team), "False, test_team is another Team with name new-team");
+        test_team.setName("test-team");
+        assertTrue(team.equals(test_team), "True, test_team has same values as Team");
+        test_team.addMember("Tristan");
+        assertFalse(team.equals(test_team), "False, test_team does not have the same values as Team");
+    }
+
+    @Test
+    public void hashCode_returns_correct_int_value() {
+        int result = team.hashCode();
+        int expectedResult = -1226298695;
+        assertEquals(expectedResult, result);
     }
 
    
